0a44ad28f6a6abede221c7feed0a54edecaabc1c
Ronaldo Sierra <sierraviloria10@gmail.com>
2025-06-03 22:14:11 -0500

    Subida

---
+++ Themes/Themes.js
@@ -0,0 +1,8 @@
+
+function mostrarModal() {
+    document.getElementById('modal-container').style.display = 'flex';
+}
+
+function cerrarModal() {
+    document.getElementById('modal-container').style.display = 'none';
+}
